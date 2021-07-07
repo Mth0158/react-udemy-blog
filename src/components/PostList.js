a16ad2d51ffd3a5d8a4d49c5513ace20fd7c530e
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-07-07 16:09:04 +0200

    Finished blog app

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts, fetchUser } from "../actions";
+import { fetchPosts, fetchUser, fetchPostsAndUsers } from "../actions";
 import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -36,4 +36,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchPosts, fetchUser })(PostList);
+export default connect(mapStateToProps, {
+  fetchPosts,
+  fetchUser,
+  fetchPostsAndUsers,
+})(PostList);
